87b4c4bf32cff337286a5e49fd96f8e28c2c81e5
kanopo <dmitri.ollari@protonmail.com>
2023-03-10 15:23:43 +0100

    added css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import remarkToc from "remark-toc";
+import {rehypeAccessibleEmojis} from "rehype-accessible-emojis";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    drafts: true,
+    remarkPlugins: [[remarkToc, {heading: "Indice"}], rehypeAccessibleEmojis],
+  },
   integrations: []
 });
--- src/pages/wiki/test.md
+++ src/pages/wiki/test.md
@@ -1,12 +1,17 @@
 ---
 title: "Test 1"
+description: "Primo post di prova di dmodmo"
 author: "me"
 tags: ["test1", "test2"]
-draft: true
+draft: false
 layout: "../../layouts/WikiPostLayout.astro"
 ---
 
+```js
+let swag = 0;
+```
+## Indice
 
-# Primo post di prova
-
-primo pos swag
+## Titolo 1
+I campi della table of content vengono auto generati
+## blu
